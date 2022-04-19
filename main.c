78edf0d4d66ea66e96fd6e9402aee97f08116fd7
VICTORALI1 <ogollavicta@gmail.com>
2022-04-19 10:39:57 -0700

    Lets see if this solves the issue

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-
     int len;
     int len2;
     unsigned int ui;
@@ -40,6 +39,6 @@ int main(void)
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
-    //printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
     return (0);
 }
